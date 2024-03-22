a25062839352d0e04b68b31fe3012d54c8d4dba8
sruthyvinod <sruthyvinod2000gmail.com>
2024-03-22 14:54:14 +0530

    Feat: Add IconButton and Navbar component

---
+++ src/components/IconButton.jsx
@@ -0,0 +1,11 @@
+
+
+const IconButton = ({children}) => {
+  return(
+     <div className="p-3 rounded-full hover:bg-primary-100 cursor-pointer">
+      {children}
+    </div>
+  );
+};
+
+export default IconButton;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import Brand from "./Brand";
+import IconButton from "./IconButton";
+import {IoSearch,IoPerson, IoBagAdd, IoBag} from "react-icons/io5";
+import Button from "./Button";
+
+const Navbar = () => {
+  const NAV_ITEMS = ["Shop All", "New" ,"Sales", "Reviews", "About Us"];
+  return (
+     <div className="p-3 flex items-center justify-between border-b">
+      <Brand/>
+      <div className="flex items-center justify-between gap-3">
+        {NAV_ITEMS.map((navItem) =>(
+          <h2 className="text-slate-500 hover:text-primary-400 cursor-pointer hover:underline">
+            {navItem}
+          </h2>
+        ))}
+      </div>
+     <div className="flex items-center gap-1">
+     <IconButton>
+        <IoSearch/>
+      </IconButton>
+      <IconButton>
+        <IoPerson/>
+      </IconButton>
+      <Button icon={ <IoBag/>} variant="text">
+       Cart
+      </Button>
+     </div>
+    </div>
+  );
+  
+};
+
+export default Navbar;
