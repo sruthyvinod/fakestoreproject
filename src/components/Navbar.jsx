74a369b247d5714e200ad8e0752546ff88a35d6c
sruthyvinod <sruthyvinod2000gmail.com>
2024-03-22 16:00:07 +0530

    Feat: Add Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,35 @@ import Brand from "./Brand";
 import IconButton from "./IconButton";
 import {IoSearch,IoPerson,IoBag} from "react-icons/io5";
 import Button from "./Button";
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
-  const NAV_ITEMS = ["Shop All", "New" ,"Sales", "Reviews", "About Us"];
+  const NAV_ITEMS = [
+    {title:"Products", to:"/products"},
+    {title:"Shop All", to:"/shop-all"},
+    {title:"New" ,to:"/new"},
+    {title:"Sales", to:"/sales"},
+    {title:"Reviews",to:"/reviews"},
+    {title:"About Us",to:"/about"},
+  ];
   return (
      <div className="p-3 flex items-center justify-between border-b">
       <Brand/>
       <div className="flex items-center justify-between gap-3">
         {NAV_ITEMS.map((navItem) =>(
-          <h2 className="text-slate-500 hover:text-primary-400 cursor-pointer hover:underline">
-            {navItem}
-          </h2>
-        ))}
+          <NavLink 
+          key={navItem.to}
+            to={navItem.to}
+            className={({isActive})=>
+              isActive
+              ? "text-primary-500 hover:text-primary-600 cursor-pointer"
+              : "text-slate-500 hover:text-primary-400 cursor-pointer hover:underline"}
+              >
+                 {navItem.title}
+              </NavLink> 
+       ))}
       </div>
-     <div className="flex items-center gap-1">
+     <div className="flex items-center">
      <IconButton>
         <IoSearch/>
       </IconButton>
