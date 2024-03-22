9b76cdbac4e6936010c7d8ef057336ce3906fee8
sruthyvinod <sruthyvinod2000gmail.com>
2024-03-22 14:52:23 +0530

    Feat: Add Brand component

---
+++ src/components/Brand.jsx
@@ -0,0 +1,12 @@
+
+
+const Brand = () => {
+  return (
+     <div className="flex items-center gap-3">
+      <img src="/assets/logo.svg" alt="logo"/>
+      <h1 className="text-primary-500 text-lg">Fakestore.io</h1>
+    </div>
+  ); 
+};
+
+export default Brand;
