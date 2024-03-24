4594547019d501b762c6ce85f1e37684caa885d9
sruthyvinod <sruthyvinod2000gmail.com>
2024-03-24 12:27:52 +0530

    Feat: Add Navbar in ProductList

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,12 @@
+import Page from "../components/Page";
+import Navbar from "../components/Navbar";
+
+const ProductList = () => {
+  return (
+  <Page>
+    <Navbar/>
+  </Page>
+  );
+};
+
+export default ProductList;
