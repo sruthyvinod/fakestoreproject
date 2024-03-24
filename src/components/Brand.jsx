835504505e9f632b8b4845a939b6ed3f1322bd5f
sruthyvinod <sruthyvinod2000gmail.com>
2024-03-24 12:23:57 +0530

    Style: Adjust Brand Colors

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -4,7 +4,7 @@ const Brand = () => {
   return (
      <div className="flex items-center gap-3">
       <img src="/assets/logo.svg" alt="logo"/>
-      <h1 className="text-primary-500 text-lg">Fakestore.io</h1>
+      <h1 className="text-gray-900  text-lg">Fakestore.io</h1>
     </div>
   ); 
 };
