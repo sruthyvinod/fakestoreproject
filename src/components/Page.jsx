e72a323796d13248ae7eed2074aaafec17abedff
sruthyvinod <sruthyvinod2000gmail.com>
2024-03-25 21:43:59 +0530

    Feat: Add default flex column direction to page and add gap

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,7 +1,7 @@
 
 
 const Page = ({children}) => {
-  return <div className="w-dvw min-h-dvh p-4">{children}</div>
+  return <div className="w-dvw min-h-dvh p-4 flex flex-col gap-5">{children}</div>
   
 };
 
